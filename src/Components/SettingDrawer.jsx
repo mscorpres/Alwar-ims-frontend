d215c1c228fc56f4385c5eaf949016c8f425696b
sanchitvarshney <sanchit.varshney@mscorpres.in>
2026-01-22 17:00:21 +0530

    Add Backend Configuration on url change

--- src/Components/SettingDrawer.jsx
+++ src/Components/SettingDrawer.jsx
@@ -4,7 +4,12 @@ import SelectSocketEndPoint from "../Pages/SelectSocketEndPoint";
 
 const SettingDrawer = ({ open, hide }) => {
   return (
-    <Drawer title="IMS Settings" open={open} onClose={hide}>
+    <Drawer 
+      title="IMS Settings" 
+      open={open} 
+      onClose={hide}
+      width={1000}
+    >
       <Typography.Title level={5}>Add Custom URL</Typography.Title>
       <SelectEndPoint />
       <Divider />
