1781bcda4faaa1f88e1c04595191e16d36cc6b81
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-07-11 11:14:05 +0530

    fix import issue

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,8 @@ import {
 } from "./utils/postLoginRedirect.js";
 import ModuleSearch from "./Components/ModuleSearch/ModuleSearch.jsx";
 import NotificationPromptDialog from "./Components/NotificationPromptDialog.jsx";
-import { registerFCMToken } from "./utils/fcmToken.js";
+import { registerFCMToken } from "./firebase/registerFCM.js";
+
 
 const App = () => {
   const { showToast } = useToast();
