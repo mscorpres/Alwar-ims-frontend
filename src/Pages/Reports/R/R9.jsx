957aed0a8d5020adbb12d42ded30ff2ad0ceafc8
sanchitvarshney <sanchit.varshney@mscorpres.in>
2026-09-08 14:43:24 +0530

    modify code

--- src/Pages/Reports/R/R9.jsx
+++ src/Pages/Reports/R/R9.jsx
@@ -304,7 +304,7 @@ const getDataByLocation = async (e) => {
         </Col>
         <Col span={19}>
           <Row>
-            {resData.length > 1 && (
+            {resData.length > 0 && (
               <Col span={24}>
                 <div
                   style={{
